217805ef668bbabe397ad956541e39aa6020f4ec
chrisblossom <cbloss@gmail.com>
2016-12-06 14:02:14 -0800

    dog food own eslint

--- packages/babel-preset-react/index.js
+++ packages/babel-preset-react/index.js
@@ -1,20 +1,7 @@
 'use strict';
 
-var es2015Parameters = require('babel-plugin-transform-es2015-parameters');
-var es2015Destructuring = require('babel-plugin-transform-es2015-destructuring');
-var objectRestSpread = require('babel-plugin-transform-object-rest-spread');
-var es2015ModulesCommonjs = require('babel-plugin-transform-es2015-modules-commonjs');
-
-function preset(context, opts) {
-    var loose = opts && !!opts.loose;
-    var useBuiltIns = (opts && opts.useBuiltIns) ? !!opts.useBuiltIns : true;
-
-    var plugins = [
-        es2015Parameters,
-        [es2015Destructuring, { loose: loose }],
-        [objectRestSpread, { useBuiltIns: useBuiltIns }],
-        [es2015ModulesCommonjs, { loose: loose }]
-    ];
+function preset() {
+    var plugins = [];
 
     return {
         plugins: plugins
