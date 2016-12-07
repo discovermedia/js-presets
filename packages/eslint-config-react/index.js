ebbe682df45e72834353c8cfe9b22eea50934751
chrisblossom <cbloss@gmail.com>
2016-12-07 09:50:50 -0800

    react eslint stuff

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -4,6 +4,31 @@ module.exports = {
     parser: 'babel-eslint',
     'extends': ['airbnb', '@discovermedia/eslint-config-es6'],
     rules: {
-        // Add more here later
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-indent': ['error', 4],
+
+        // Too many errors/unreliable
+        'react/no-unused-prop-types': 0,
+
+        'react/jsx-filename-extension': [2, { extensions: ['.js'] }],
+
+        // propTypes were out of order
+        'react/sort-comp': [
+            'error', {
+                order: [
+                    'displayName',
+                    'propTypes',
+                    'contextTypes',
+                    'childContextTypes',
+                    'mixins',
+                    'static-methods',
+                    '/^on.+$/',
+                    '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
+                    'everything-else',
+                    '/^render.+$/',
+                    'render'
+                ]
+            }
+        ]
     }
 };
